2a6f92afe48082d083748bb07e859700a494d537
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-06-06 23:13:23 +0000

    Add dev.sh

---
+++ dev.sh
@@ -0,0 +1,10 @@
+docker run -ti --rm \
+	-v $PWD:/home/aghost-7/jira-releaser \
+	-w /home/aghost-7/jira-releaser \
+	-v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK \
+	-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+	-v $HOME/.gitconfig:/home/aghost-7/.gitconfig \
+	-e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+	aghost7/rust-dev:stable \
+	bash
